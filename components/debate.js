73b18cc6ebfcb4722305c67965141555356c7945
edenadler <eden.n.adler@gmail.com>
2017-03-21 10:14:16 +0200

    combining debate components, working on tooltip

--- components/debate.js
+++ components/debate.js
@@ -2,11 +2,6 @@ var React = require('react');
 var io = require("socket.io-client");
 var $ = require('jquery');
 var DebateInfo = require('../components/DebateInfo');
-var VotePro = require('../components/votepro');
-var VoteCon = require('../components/votecon');
-var DebaterCon = require('../components/debatercon');
-var DebaterPro = require('../components/debaterpro');
-
 
 var Debate = React.createClass({
 	getInitialState: function(){
@@ -15,10 +10,7 @@ var Debate = React.createClass({
 			round:this.props.debateSettings.rounds[0].round,
 			firstTime:true,
 			event:this.props.debateSettings.rounds[0].event,
-			timeUntilNextRound: 5,
-			selections: [false, false],
-			firstVote: true,
-
+			timeUntilNextRound: 5
 			
 
 		}
@@ -102,48 +94,12 @@ var Debate = React.createClass({
 		} //round "segment" finished eg: timer under 0	
 	}); //initial countdown
 	}, //function "tick" closing
-	 onChildToggle: function(id, selected) {
-        var selections = this.state.selections;
-        var socket = io.connect('http://localhost:3000');
-
-          if (id == 0){
-            selections[0] = selected;
-            selections[1] = !selected;
-          }
-          else if (id == 1){
-            selections[1] = selected;
-            selections[0] = !selected;
-          }
-    
-        socket.emit('vote', {selections: selections, first: this.state.firstVote});
-
-        this.setState({
-            roundTimeLeft:this.props.debateSettings.rounds[this.state.round].time,
-            round:this.state.round,
-            firstTime: this.state.firstTime,
-            selections: selections,
-            firstVote: false
-        });
-        
-    },
-
 	render:function(){
 		return(
-
-            <div>
-                <div className="debate-title"><h1>{this.props.debateSettings.topic}</h1></div>
-                <DebateInfo roundTimeLeft = {this.state.roundTimeLeft} startDebate = {this.startDebate} timeUntilNextRound = {this.state.timeUntilNextRound} round = {this.state.round} event ={this.state.event}/>
-              <div className="debate row">
-                <div className="debater con col-md-4 col-md-offset-2 text-center">
-                    <Debater name={this.props.con.name} side={this.props.con.side} index="2" location={this.props.con.location} level={this.props.con.level} followers={this.props.con.followers}/>
-                    <VoteCon id = "1" selected={this.state.selections[1]} onToggle={this.onChildToggle}/> 
-                </div>
-                <div className="debater pro col-md-4 col-md-offset-2 text-center">
-                    <Debater name={this.props.pro.name} side={this.props.pro.side} index="1" location={this.props.pro.location} level={this.props.pro.level} followers={this.props.pro.followers}/>
-                    <VotePro id = "0" selected={this.state.selections[0]} onToggle={this.onChildToggle}/>
-                </div>
-                </div>
-            </div>
+				<div className="debate-wrapper container">
+					<div className="debate-title"><h1>{this.props.debateSettings.topic}</h1></div>
+					<DebateInfo roundTimeLeft = {this.state.roundTimeLeft} startDebate = {this.startDebate} timeUntilNextRound = {this.state.timeUntilNextRound} round = {this.state.round} event ={this.state.event}/>
+				</div>
 				
 			)
 	}
