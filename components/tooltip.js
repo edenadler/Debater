46378a6f57c6ac6e46f4a9f5a84ee382abbb16c8
edenadler <eden.n.adler@gmail.com>
2017-03-22 01:41:24 +0200

    working on moving tooltip, almost there

--- components/tooltip.js
+++ components/tooltip.js
@@ -7,15 +7,13 @@ var Tooltip = React.createClass({
     return(
             <div className="debaters">
                <div className="tooltip-container">
-                  <div className="debater-name-side">
                     <a className="debater-name">{this.props.details.name}</a>
                     <span className="debater-side">{this.props.side}</span>
-                      <div className="name-tooltip">
-                        <p><i className="fa fa-globe icon" aria-hidden="true"/>{this.props.details.location}</p>
-                        <p><i className="fa fa-certificate icon" aria-hidden="true"/>{this.props.details.level}</p>
-                        <p><i className="fa fa-users icon" aria-hidden="true"/>{this.props.details.followers}</p>
-                      </div>
-                  </div>
+                    <div className="name-tooltip">
+                      <p><i className="fa fa-globe icon" aria-hidden="true"/>{this.props.details.location}</p>
+                      <p><i className="fa fa-certificate icon" aria-hidden="true"/>{this.props.details.level}</p>
+                      <p><i className="fa fa-users icon" aria-hidden="true"/>{this.props.details.followers}</p>
+                    </div>
                 </div>
             </div>
       )
